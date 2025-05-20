64eed110eb8d99501400b552d10274d86ce68ebc
Joe bae <lineanity@gmail.com>
2025-05-20 07:22:41 +0700

    minor fix

--- app/.well-known/farcaster.json/route.ts
+++ app/.well-known/farcaster.json/route.ts
@@ -2,13 +2,13 @@ export async function GET() {
 
     const config = {
         accountAssociation: {
-            header: "eyJmaWQiOjg5MTkxNCwidHlwZSI6ImN1c3RvZHkiLCJrZXkiOiIweDRmYzg1YjUzN2FkYzE4RmYzNTRhMzJDNkUxM0JCRGNEZDk0YTZEMDEifQ",
+            header: "eyJmaWQiOjk1MDY4OCwidHlwZSI6ImN1c3RvZHkiLCJrZXkiOiIweGJBNWVkNjVERjdGMzgyNWZEODk4NzQ5MTk0YjA3MTEyQzIwOWQxOWEifQ",
             payload: "eyJkb21haW4iOiJzY3JhdGNoZXMudmVyY2VsLmFwcCJ9",
-            signature: "MHgxMGZkNTVlNGQwNDA0MmU2M2Q3NjdjZGZjMTRhMmJhYmFhYjE5Y2JkNjQ1OGVhMjFmMWE3YTM0ZDMzZjlkZTMxMWM5N2Y1M2RjZjA1MzhiNmM1NTc0MGMwZDVlYzMwMDg1MDE5ODA3MGI2MDJjYTFkM2ZkMjBiZTE0ODQxNTUxMzFi"
+            signature: "MHg2MmVhZTBjMTllNTQ4YWUyMzNhOWQwYTA5ZDhiYjQ5N2M0NzQxMjA4NWRkN2JjNDljY2I2YmFkMmVhOTJmZmM2M2NlODZmYTUwNGE2N2ZjMGU5ZjBiYmYzYTU1MDhkOTdkNzM1YjhiYTFjNmUwZTQ1Njk5ZDUxOTM1OGFkYmZlMDFj"
         },
         frame: {
             version: "1",
-            name: "Scratches 🖌 Scratch of Art",
+            name: "scratches",
             iconUrl: "https://scratches.vercel.app/scratch.png",
             homeUrl: "https://scratches.vercel.app",
             imageUrl: "https://scratches.vercel.app/og-image.jpg",
